b93da0405a7af8bbc640c30bb55bc6ea4aee152f
Pompinchuz <huillajesusantonio@gmail.com>
2025-11-30 21:54:07 -0500

    redeploy

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'http://localhost:8080/api',
     uploadUrl: 'http://localhost:8080/uploads',
     appName: 'Constructora Sistema de Proformas',
     version: '1.0.0'
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { environment } from './environments/environment';
+
 
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
