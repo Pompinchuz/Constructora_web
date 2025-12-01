299e0f8562e4c17b1c97d8d4d926f922f96e192b
Pompinchuz <huillajesusantonio@gmail.com>
2025-11-30 20:35:27 -0500

    fix: Deshabilitar SSR para deployment en Netlify sss

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,7 +5,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
+//import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
+    //provideClientHydration(),
     provideHttpClient(
       withFetch(),  // ← ESTO ELIMINA LA ADVERTENCIA
       withInterceptors([authInterceptor])
